d798894366bbbafa603aa9f8430573824e984ec0
Michelle Parker <mp18@sanger.ac.uk>
2013-02-19 12:53:11 +0000

    Completed. Need to test and comment.

--- Code/v3.py
+++ Code/v3.py
@@ -41,8 +41,6 @@ def nodesplit(G,n,l,level):
 
 #Function which tests if two nodes are similar enough to merge. Returns True or False.
 def mergetest(a,b):
-    print "a,b"
-    print a,b
     #Score threshold and haplotype count for nodes a and b are calculated.
     totala = sum(G.node[a]['frequency'])
     totalb = sum(G.node[b]['frequency'])
@@ -57,158 +55,95 @@ def mergetest(a,b):
     B=nx.DiGraph()
     B.clear()
     B.add_node(1,haplotype=G.node[b]['haplotype'],frequency=G.node[b]['frequency'])
-    #for i in range(len(G.node[a]['haplotype'][0])-1 ):
-    print 'Tree A nodes'
-    for i in A.nodes_iter(data=True):
-        print i
-    print 'alevel'
-    print alevel
-    print 'Tree B nodes'
-    for i in B.nodes_iter(data=True):
-        print i
-    print 'blevel'
-    print blevel
         
     q = [[1,1,0]]
 
     while len(q) != 0:
-        print "nodesplit A and B"
-        print q[0][0],q[0][1],q[0][2]
         nodesplit(A,q[0][0],q[0][2],alevel)
         nodesplit(B,q[0][1],q[0][2],blevel)
-
-        print 'Tree A edges'
-        for i in A.edges(data=True):
-            print i
-        print 'Tree A nodes'
-        for i in A.nodes_iter(data=True):
-            print i
-        print 'alevel'
-        print alevel
-
-        print 'Tree B edges'
-        for i in B.edges(data=True):
-            print i
-        print 'Tree B nodes'
-        for i in B.nodes_iter(data=True):
-            print i
-        print 'blevel'
-        print blevel
         
         dicta = dict((edata['allele'],sum(A.node[v]['frequency'])) for u,v,edata in A.out_edges(q[0][0], data=True))
         dictb = dict((edata['allele'],sum(B.node[v]['frequency'])) for u,v,edata in B.out_edges(q[0][0], data=True))
         
         seta = set(dicta.keys())
         setb = set(dictb.keys())
-        print "seta, setb"
-        print seta, setb
         
         for i in list(seta.symmetric_difference(setb)):
             if i in dicta:
-                print "in dicta"
+
                 score = math.fabs(float(dicta[i])/float(totala))
                 if score > threshold:
-                    print "merge test false: allele not in both outgoing edges"
+
                     return False
                 elif score > maxscore:
                     maxscore = score
 
             if i in dictb:
-                print "in dictb"
+
                 score = math.fabs(float(dictb[i])/float(totalb))
                 if score > threshold:
-                    print "merge test false: allele not in both outgoing edges"
+    
                     return False
                 elif score > maxscore:
                     maxscore = score
                    
         for i in list(seta.intersection(setb)):
             score = math.fabs(float(dicta[i])/totala - (float(dictb[i])/totalb))
-            if score > threshold:
-                print "merge test false: allele in both outgoing edges"
+            if score > threshold:   
                 return False
                 
             else:
-                print "edge test true"
                 if score > maxscore:
-                    maxscore = score
-                print "i"
-                print i
+                    maxscore = score          
                
-                print A.out_edges(q[0][0],data=True)
                 a = [v for u,v,edata in A.out_edges(q[0][0],data=True) if edata['allele']==i]
-                print a
-                
+                               
                 b = [v for u,v,edata in A.out_edges(q[0][0],data=True) if edata['allele']==i]
-                print b
-
-                    
-                #for u,v,edata in B.out_edges(q[0][1],data=True) if edata['allele']=='i':
-                #print u,v,edata
+                                  
                 if len(A.node[a[0]]['haplotype'][0]) != 0:
-                    print "q.append"
-                    print a[0],b[0],q[0][2]+1
                     q.append([a[0] ,b[0] ,q[0][2]+1])
-                print "q is"
-                print q
-            
+
         q.pop(0)
-        print "End: q is"
-        print q
-        
-    print "return maxscore"  
+  
     return maxscore
 
 def mergenodes(a,b):
-    print 'Edges in Graph'
-    for i in G.edges(data=True):
-        print i
-    print 'Nodes in Graph'
-    for i in G.nodes_iter(data=True):
-        print i
-    print 'glevel'
-    print glevel
+    for i in range(len(G.node[b]['haplotype'])):
+          
+        if G.node[b]['haplotype'][i] in [j for j in G.node[a]['haplotype']]:
+            index = [j for j in G.node[a]['haplotype']].index(G.node[b]['haplotype'][i])
+            G.node[a]['frequency'][index] = G.node[a]['frequency'][index] + G.node[b]['frequency'][i]
 
-    for i in G.node[a]['haplotype']:
-        if i in G.node[b]['haplotype']:
-            print "yes"
+        else:
+            G.node[a]['haplotype'].append(G.node[b]['haplotype'][i])
+            G.node[a]['frequency'].append(G.node[b]['frequency'][i])
 
-    seta=[i for i in G.node[a]['haplotype']]
-    setb=[i for i in G.node[b]['haplotype']]
-    print seta
-    print setb
+    for i in G.in_edges(b, data=True):
+        G.add_edge(i[0],a,allele=i[2]['allele'])
+        G.remove_edge(i[0],i[1])
 
-    for i in list(seta.symmetric_difference(setb)):
-        print i
-        #only if summetric_difference exists!
+    G.remove_node(b)
 
-    for i in list(seta.intersection(setb)):
-        print i
-
-    #add frequencies on to same haplotypes
-    #move edge to second node to the first node.
-    #delete last node and edit level.
-   
+    #Edit glevel!!!
+    for i in glevel:
+        if b <= i:
+            glevel[glevel.index(i)] = glevel[glevel.index(i)] - 1
 
 def merge(l):
     nn = glevel[l] - glevel[l-1]
-    if nn == 1:
-        print "one node in level so no merge can be performed"
+    if nn == 1:     
         pass
     
     #If there is only 2 nodes in level. Test similarity between the pair of nodes.
     elif nn == 2:
-        print "2 nodes in level"
                
         if mergetest(glevel[l],glevel[l]-1) == False:
             pass
         
         else:
-            print "nodes need to be merged"
             mergenodes(level[i],level[i]-1)
 
     else:
-        print "more than two nodes in level"        
         merge = True
         while merge == True:
             merge = False
@@ -222,8 +157,7 @@ def merge(l):
                     
                     if j != k:
                         testscore = mergetest(j,k)
-                        print "j,k,testscore"
-                        print j,k,testscore
+
                  
                         if testscore == False:
                             continue
@@ -234,8 +168,6 @@ def merge(l):
                                 mink = k
                                             
             if levelmin != 1000:
-                print "merge nodes"
-                print minj,mink
                 mergenodes(minj,mink)
                 merge = True
 
@@ -249,26 +181,14 @@ G=nx.MultiDiGraph()
 
 #Add start node 1
 G.add_node(1,haplotype=haplotype,frequency=frequency)
-
-#Split node 1 of graph G.
-print "nodesplit(G,1,0,glevel)"
 nodesplit(G,1,0,glevel)
 
+for i in range(1,haplolength):
+    merge(i)
 
-
-
-#Carry out merge part on all nodes in level 1
-print "merge(1)"
-merge(1)
-
-#for i in range(glevel[l-1]+1,glevel[l]+1):
-#nodesplit(G,i,l)
-
-
-print "nodesplit(G,2,i,glevel) i=[1,2]"
-for i in range(glevel[0]+1,glevel[1]+1):
-    nodesplit(G,i,1,glevel)
-
+    for j in range(glevel[i-1]+1,glevel[i]+1):
+        nodesplit(G,j,i,glevel)
+    
 print 'Edges in Graph'
 for i in G.edges(data=True):
     print i
@@ -277,9 +197,3 @@ for i in G.nodes_iter(data=True):
     print i
 print 'glevel'
 print glevel
-
-
-print "merge(2)"
-#merge(2)
-mergenodes(4,6)
-
